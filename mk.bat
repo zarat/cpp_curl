c6c0990d5976c88d725458819b91793eaa0a130c
Manuel Zarat <manuel@zarat.ml>
2022-07-14 15:11:36 +0200

    Update mk.bat

--- mk.bat
+++ mk.bat
@@ -1,7 +1,5 @@
 del *.exe *.o
 
-@rem g++ main.cpp -Iinclude -Llib/dll-release-x86 -lcurl -lws2_32 -lwinmm
-
-g++ main.cpp -o main -Llib/dll-release-x86 -llibcurl -Iinclude
+g++ -static main.cpp -o main -Llib/dll-release-x86 -llibcurl -Iinclude
 
 pause
